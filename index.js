eb5cdbd441875e63866a55e05518ae1a170f4e23
Prateek Sharma <91019339+PrateekSharma007@users.noreply.github.com>
2023-04-17 19:42:55 +0530

    new changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const fs = require('fs');
 var requests = require('requests')
 
-const home_file = fs.readFileSync('Home.html', 'utf-8');
+const homefile = fs.readFileSync("Home.html", 'utf-8');
 
 const replaceval = (tempval,orival) =>{ 
     let temperature = tempval.replace("{%tempval%}",orival.main.temp)
@@ -16,7 +16,7 @@ const replaceval = (tempval,orival) =>{
 }
 
 const server = http.createServer((req, res) => {
-    if (req.url === "/") {
+    if (req.url == "/") {
         requests('https://api.openweathermap.org/data/2.5/weather?q=Noida&appid=9a9c791d8fd25c25bae95df7dd081207')
         .on("data" , chunk => {
             const obdata = JSON.parse(chunk)
@@ -24,15 +24,16 @@ const server = http.createServer((req, res) => {
             // console.log(arr[0].main.temp )
             console.log(arr[0].main)
             var realtimedata = arr.map((val) =>{ 
-                replaceval(home_file,val)
+                replaceval(homefile,val)
                 
             }).join("");
             // res.write(realtimedata)
-            // console.log(main.val);
+            
             console.log(realtimedata)
         })
         .on("end", function(err){
             if(err) return console.log("Connection closed due to error")
+            
             console.log('end')
             res.end();
         })
